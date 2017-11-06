67328e98d6a294aecfe97e7f86ae24c6489a0aa1
griffenliu <tiansha7xing@gmail.com>
2017-11-06 18:32:29 +0800

    custome

--- themes/walle/theme.lua
+++ themes/walle/theme.lua
@@ -356,7 +356,7 @@ function theme.at_screen_connect(s)
             -- arrow("#4B3B51", "#CB755B"),
             -- wibox.container.background(wibox.container.margin(wibox.widget { fsicon, theme.fs.widget, layout = wibox.layout.align.horizontal }, 3, 3), "#CB755B"),
 
-            arrow("#8DAA9A", "#8DAA9A"),
+            arrow(theme.bg_normal, "#8DAA9A"),
             wibox.container.background(wibox.container.margin(wibox.widget { baticon, bat.widget, layout = wibox.layout.align.horizontal }, 3, 3), "#8DAA9A"),
             arrow("#8DAA9A", "#C0C0A2"),
             wibox.container.background(wibox.container.margin(wibox.widget { nil, neticon, net.widget, layout = wibox.layout.align.horizontal }, 3, 3), "#C0C0A2"),
